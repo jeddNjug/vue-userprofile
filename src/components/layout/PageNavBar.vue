f498ff0ec70869b380eda2d6eaae6bc1a7d68cb0
Jedd <jeddmbugua@gmail.com>
2023-07-11 22:23:48 +0300

    Added fonts and their realated styles

--- src/components/layout/PageNavBar.vue
+++ src/components/layout/PageNavBar.vue
@@ -6,7 +6,7 @@ defineEmits(['update:modelValue'])
     <nav class="bg-white border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="https://vuejs.org/" class="flex items-center">
-                <img src="https://v2.vuejs.org/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
+                <img src="https://v2.vuejs.org/images/logo.svg" class="h-8 mr-3" alt="Portal Website" />
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Portal</span>
             </a>
 
