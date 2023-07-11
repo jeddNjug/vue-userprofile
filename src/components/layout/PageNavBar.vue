07353d9ebd69f989ccd7e6dfd6faf81bad674c7a
Jedd <jeddmbugua@gmail.com>
2023-07-11 14:06:29 +0300

    Added proficiency skill gauge

---
+++ src/components/layout/PageNavBar.vue
@@ -0,0 +1,22 @@
+<script setup>
+import ThemeSwitcher from './ThemeSwitcher.vue';
+</script>
+<template>
+    <nav class="bg-white border-gray-200 dark:bg-gray-900">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="https://vuejs.org/" class="flex items-center">
+                <img src="https://v2.vuejs.org/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Portal</span>
+            </a>
+
+            <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
+                <ul
+                    class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <li>
+                        <theme-switcher></theme-switcher>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
